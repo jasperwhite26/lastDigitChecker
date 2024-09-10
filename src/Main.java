12b5a2dea8b11dfc77ef4511ccaff278e00b1f7e
jasper <whiteana001@warren.k12.in.us>
2024-09-10 13:02:07 -0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(hasSameLastDigit(41, 22, 71));
+        System.out.println(hasSameLastDigit(23, 32, 42));
+        System.out.println(hasSameLastDigit(9, 99, 999));
+    }
+
+    private static boolean hasSameLastDigit(int num1, int num2, int num3) {
+        if(isValid(num1)){
+            return false;
+        }
+        if(isValid(num2)){
+            return false;
+        }
+        if(isValid(num3)){
+            return false;
+        }
+
+        int num1Last = num1 % 10;
+        int num2Last = num2 % 10;
+        int num3Last = num3 % 10;
+
+        if(num1Last == num2Last){
+            return true;
+        } else if(num2Last == num3Last){
+            return true;
+        } else if(num1Last == num3Last) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isValid(int num) {
+        return num < 10 || num > 100;
+    }
 }
